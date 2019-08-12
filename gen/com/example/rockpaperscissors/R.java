d91a82ddbb22698de0495974d0a8c5d9d7e33b04
Akshit Gupta <saishetty26@gmail.com>
2019-08-11 23:17:38 -0400

    Deleted Unused files and scores screen

--- gen/com/example/rockpaperscissors/R.java
+++ gen/com/example/rockpaperscissors/R.java
@@ -36,11 +36,10 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_modes=0x7f030000;
-        public static final int activity_scores=0x7f030001;
-        public static final int activity_settings=0x7f030002;
-        public static final int activity_signin=0x7f030003;
-        public static final int activity_signup=0x7f030004;
-        public static final int activity_splash=0x7f030005;
+        public static final int activity_settings=0x7f030001;
+        public static final int activity_signin=0x7f030002;
+        public static final int activity_signup=0x7f030003;
+        public static final int activity_splash=0x7f030004;
     }
     public static final class menu {
         public static final int activity_login=0x7f060000;
