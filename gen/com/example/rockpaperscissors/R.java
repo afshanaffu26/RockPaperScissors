ceada5b06c44c0bac1436575a829396f7b921956
Akshit Gupta <afshanaffu26@gmail.com>
2019-08-11 23:26:58 -0400

    Deleted Unused resources

--- gen/com/example/rockpaperscissors/R.java
+++ gen/com/example/rockpaperscissors/R.java
@@ -14,9 +14,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int rps=0x7f020001;
         public static final int rps_adv=0x7f020002;
-        public static final int rps_bg=0x7f020003;
-        public static final int rps_classic=0x7f020004;
-        public static final int rpsls=0x7f020005;
+        public static final int rps_classic=0x7f020003;
+        public static final int rpsls=0x7f020004;
     }
     public static final class id {
         public static final int button1=0x7f070006;
