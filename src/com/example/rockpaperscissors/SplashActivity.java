dbd0a2eef55cf8622edf3f80d890a029af91ca3c
afshanaffu26 <afshanaffu26@gmail.com>
2019-08-17 03:43:28 -0400

    Added logic for splash screen and renamed signup activity

--- src/com/example/rockpaperscissors/SplashActivity.java
+++ src/com/example/rockpaperscissors/SplashActivity.java
@@ -1,15 +1,34 @@
 package com.example.rockpaperscissors;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class SplashActivity extends Activity {
+	
+	private static int SPLASH_TIME_OUT = 3000;
+	private boolean isBackPressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+            	if(!isBackPressed){
+                Intent i = new Intent(SplashActivity.this, SigninActivity.class);
+                startActivity(i);
+            	}
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
     @Override
@@ -18,5 +37,10 @@ public class SplashActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_splash, menu);
         return true;
     }
+    
+    @Override
+	   public void onBackPressed() {
+		 isBackPressed = true;
+	   }
     
 }
