a469414e51029b6511a625ee96f340bee0458201
Saikrishna Samudrala <saishetty26.sk@gmail.com>
2019-08-18 11:40:36 -0400

    Added logic to settings screen to control the music from application.

--- src/com/example/rockpaperscissors/SettingsActivity.java
+++ src/com/example/rockpaperscissors/SettingsActivity.java
@@ -1,15 +1,25 @@
 package com.example.rockpaperscissors;
 
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends Activity implements OnSeekBarChangeListener {
+	
+	AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		SeekBar volumeSlider = (SeekBar) findViewById(R.id.volumeSlider);
+		volumeSlider.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+		volumeSlider.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)); 
+        volumeSlider.setOnSeekBarChangeListener(this);
 	}
 
 	@Override
@@ -17,6 +27,25 @@ public class SettingsActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_settings, menu);
 		return true;
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		// TODO Auto-generated method stub
+		audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress, 0);
+		
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
