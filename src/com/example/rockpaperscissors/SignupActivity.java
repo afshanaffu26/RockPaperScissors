1a09b36e8f2fb7a512229431b76c1bf82cc9acaa
Akshit Moturi <akshitcool28@gmail.com>
2019-08-18 22:59:46 -0400

    modified signupActivity.java , Added logic to add user to databse.

--- src/com/example/rockpaperscissors/SignupActivity.java
+++ src/com/example/rockpaperscissors/SignupActivity.java
@@ -1,15 +1,36 @@
 package com.example.rockpaperscissors;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.text.Editable;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUpActivity extends Activity {
+public class SignUpActivity extends Activity implements OnClickListener {
+	
+	static SQLiteDatabase db=null;
+	EditText nameInput,usernameInput, passwordInput, reEnterPasswordInput;
+	Button signupBtn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signup);
+		nameInput = (EditText) findViewById(R.id.nameInput);
+		usernameInput = (EditText) findViewById(R.id.usernameInput);
+		passwordInput = (EditText) findViewById(R.id.passwordInput);
+		reEnterPasswordInput = (EditText) findViewById(R.id.reEnterPasswordInput);
+		signupBtn = (Button) findViewById(R.id.signup);
+		DbHelper dbh=new DbHelper(this, "score.db", null, 1);
+		db=dbh.getWritableDatabase();
+		signupBtn.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +38,28 @@ public class SignUpActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_signup, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		ContentValues cv = new ContentValues();
+		String name = nameInput.getText().toString();
+		String username = usernameInput.getText().toString();
+		String password = passwordInput.getText().toString();
+		String reEnterPassword = reEnterPasswordInput.getText().toString();
+		if(password.equals(reEnterPassword)){
+			cv.put("name", name);
+			cv.put("username", username);
+			cv.put("password", password);
+			db.insert("userDetails", null, cv);
+			
+		}else{
+			Toast.makeText(getApplicationContext(),"Your passwords doesn't match",Toast.LENGTH_SHORT).show();
+			passwordInput.setText("");
+			reEnterPasswordInput.setText("");
+		}
+		
 	}
 
 }
