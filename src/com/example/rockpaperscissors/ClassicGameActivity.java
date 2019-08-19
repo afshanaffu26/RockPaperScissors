e15ea6c0eec4b39bf5ca9baef3eb4815e6ee82d6
afshanaffu26 <afshanaffu26@gmail.com>
2019-08-19 06:55:15 -0400

    Adding onclick listeners and ENUM constants

--- src/com/example/rockpaperscissors/ClassicGameActivity.java
+++ src/com/example/rockpaperscissors/ClassicGameActivity.java
@@ -1,22 +1,69 @@
 package com.example.rockpaperscissors;
 
+import java.util.Random;
+
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ClassicGameActivity extends Activity {
+public class ClassicGameActivity extends Activity implements OnClickListener {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_classic_game);
-	}
+    public enum Option {
+        ROCK,
+        PAPER,
+        SCISSORS
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_classic_game, menu);
-		return true;
-	}
+    public enum Result {
+        WIN,
+        LOSS,
+        DRAW
+    }
+    
+    static SQLiteDatabase db=null;
+    private Option userSelection, androidSelection;
+    int uc = 0;
+    int ac = 0;
+    Button bt1, bt2, bt3;
+    ImageButton bt4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classic_game);
+        DbHelper dbh = new DbHelper(this, "game.db", null, 1);
+        db = dbh.getWritableDatabase();
+        bt1 = (Button) findViewById(R.id.rock);
+        bt2 = (Button) findViewById(R.id.paper);
+        bt3 = (Button) findViewById(R.id.scissors);
+        bt4 = (ImageButton) findViewById(R.id.imageButtonHome);
+        bt1.setOnClickListener(this);
+        bt2.setOnClickListener(this);
+        bt3.setOnClickListener(this);
+        bt4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+
+       
+    }
+
+   
 
 }
